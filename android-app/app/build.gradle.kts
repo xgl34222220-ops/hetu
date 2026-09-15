dc9afd7d80ba0154d91c2bdb74523ba63a501a9d
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-15 11:57:36 +0800

    Bump bundled config preview to test.23

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.xgl34222220.bichen"
         minSdk = 26
         targetSdk = 35
-        versionCode = 422
-        versionName = "0.4.0-test.22"
+        versionCode = 423
+        versionName = "0.4.0-test.23"
     }
 
     buildFeatures {
