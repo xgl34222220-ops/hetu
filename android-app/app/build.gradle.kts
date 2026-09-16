f8b15789e26b6828f0a463e62a4a4fef65dabfee
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-16 17:38:21 +0800

    Bump Bichen proxy to 0.4.0-test.27

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.xgl34222220.bichen"
         minSdk = 26
         targetSdk = 35
-        versionCode = 426
-        versionName = "0.4.0-test.26"
+        versionCode = 427
+        versionName = "0.4.0-test.27"
     }
 
     buildFeatures {
