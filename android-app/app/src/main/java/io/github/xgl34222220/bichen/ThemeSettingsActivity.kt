953f007fc54510c1457be1ad26691343cd5e7b8e
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-16 13:55:05 +0800

    Fix theme settings list import

--- android-app/app/src/main/java/io/github/xgl34222220/bichen/ThemeSettingsActivity.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/bichen/ThemeSettingsActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
