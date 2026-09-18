3147d976174fcef34ee1214c68aeaf27f87dab61
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-18 08:58:28 +0800

    Bump Bichen to 0.4.0-test.61

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.xgl34222220.bichen"
         minSdk = 26
         targetSdk = 35
-        versionCode = 460
-        versionName = "0.4.0-test.60"
+        versionCode = 461
+        versionName = "0.4.0-test.61"
     }
 
     buildFeatures {
