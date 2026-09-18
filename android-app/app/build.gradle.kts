c61d32b8978243d316fda98c1cddeb18b43cfc0a
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-19 07:54:40 +0800

    Bump Hetu frame-review fixes to test.66

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.xgl34222220.hetu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 465
-        versionName = "0.4.0-test.65"
+        versionCode = 466
+        versionName = "0.4.0-test.66"
     }
 
     buildFeatures {
