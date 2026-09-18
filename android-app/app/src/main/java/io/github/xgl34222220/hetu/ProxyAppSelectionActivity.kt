820579a7b521b8f3a81b99f6b0be6fd73f4adb9e
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-19 07:53:08 +0800

    Unify Hetu app-list bottom safe space

--- android-app/app/src/main/java/io/github/xgl34222220/hetu/ProxyAppSelectionActivity.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/hetu/ProxyAppSelectionActivity.kt
@@ -111,7 +111,7 @@ private fun ProxyAppSelectionPage(onBack: () -> Unit) {
 
     LazyColumn(
         modifier = Modifier.fillMaxSize().background(pageBg),
-        contentPadding = PaddingValues(start = 16.dp, top = 8.dp, end = 16.dp, bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding() + 40.dp),
+        contentPadding = PaddingValues(start = 16.dp, top = 8.dp, end = 16.dp, bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding() + 92.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
         item("header") {
