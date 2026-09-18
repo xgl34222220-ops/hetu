09ec5ce8a065fa6de8a56a81139cc0914efbf04e
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-19 07:52:57 +0800

    Unify Hetu adblock bottom safe space

--- android-app/app/src/main/java/io/github/xgl34222220/hetu/ProxyAdblockChainActivity.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/hetu/ProxyAdblockChainActivity.kt
@@ -334,7 +334,7 @@ private fun ProxyAdblockChainPage(onBack: () -> Unit) {
             start = 16.dp,
             top = 8.dp,
             end = 16.dp,
-            bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding() + 80.dp,
+            bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding() + 92.dp,
         ),
         verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
