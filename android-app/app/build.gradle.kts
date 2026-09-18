8320c60ff3cae1e396a2107958cb9e3d25ed41e7
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-18 08:00:56 +0800

    Bump Bichen to 0.4.0-test.59

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.xgl34222220.bichen"
         minSdk = 26
         targetSdk = 35
-        versionCode = 458
-        versionName = "0.4.0-test.58"
+        versionCode = 459
+        versionName = "0.4.0-test.59"
     }
 
     buildFeatures {
