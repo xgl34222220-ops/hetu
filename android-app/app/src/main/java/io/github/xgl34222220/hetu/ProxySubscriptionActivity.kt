fbb40ff27b07c6faeb51120470eb49019497ff51
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-19 08:46:56 +0800

    Fix Hetu subscription clickable modifier import

--- android-app/app/src/main/java/io/github/xgl34222220/hetu/ProxySubscriptionActivity.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/hetu/ProxySubscriptionActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.horizontalScroll
