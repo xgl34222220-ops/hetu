b961a35cd6b0236ed0306f0ca3917a31f3b0641f
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-19 13:57:11 +0800

    Bump Hetu native YAML editor build to test.70

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.xgl34222220.hetu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 469
-        versionName = "0.4.0-test.69"
+        versionCode = 470
+        versionName = "0.4.0-test.70"
     }
 
     buildFeatures {
