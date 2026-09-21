ad4903e9be876206e56db31fbec15f98be13747c
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-21 12:30:18 +0800

    fix: import YAML accessory color token

--- android-app/app/src/main/java/io/github/xgl34222220/hetu/EditorWorkbench.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/hetu/EditorWorkbench.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
