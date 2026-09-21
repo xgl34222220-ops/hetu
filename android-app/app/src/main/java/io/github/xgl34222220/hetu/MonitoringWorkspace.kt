be86b7d3503dcca9c3e6ce7af0739e2c8b10fb92
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-21 14:57:06 +0800

    fix(ui): import subscription luminance helper

--- android-app/app/src/main/java/io/github/xgl34222220/hetu/MonitoringWorkspace.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/hetu/MonitoringWorkspace.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
