2e0d1a41f15b5e8b463e4726a0e4852e507237fd
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-21 10:43:22 +0800

    fix: import file badge circle shape

--- android-app/app/src/main/java/io/github/xgl34222220/hetu/ReferenceFileManagerActivity.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/hetu/ReferenceFileManagerActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
