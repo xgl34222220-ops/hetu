047b918eee6d9d043cb0b2808d2aaecf5db34e4d
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-21 10:39:32 +0800

    build: bump preview to 0.4.0-test.106

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.xgl34222220.hetu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 505
-        versionName = "0.4.0-test.105"
+        versionCode = 506
+        versionName = "0.4.0-test.106"
     }
 
     testOptions {
