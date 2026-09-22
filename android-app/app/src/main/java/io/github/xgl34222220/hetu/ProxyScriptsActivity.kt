81e930b8dab8f2d08e1293d57f528c879cca0ae6
xgl34222220-ops <xgl34222220@gmail.com>
2026-09-22 13:44:30 +0800

    fix(scripts): import lazy list for script page

--- android-app/app/src/main/java/io/github/xgl34222220/hetu/ProxyScriptsActivity.kt
+++ android-app/app/src/main/java/io/github/xgl34222220/hetu/ProxyScriptsActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
